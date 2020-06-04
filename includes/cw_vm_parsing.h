97e150ebfaf14a13530ee8c41f5715df5550f941
4slan <amalsago@student.42.fr>
2020-06-05 01:53:26 +0200

    feat(cw_vm): improve parsing of command line argumets

--- includes/cw_vm_parsing.h
+++ includes/cw_vm_parsing.h
@@ -6,24 +6,28 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 18:48:57 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/04 18:48:57 by jthierce         ###   ########.fr       */
+/*   Updated: 2020/06/05 01:29:01 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CW_VM_PARSING_H
 # define CW_VM_PARSING_H
 
+# define CW_FILENAME_MAX_LEN	128
+
 /*
 ** Data struct create with parsing
 **
-** nbr_cycles = dump the memory in nbr_cycles
-** number = number of the next players
+** nbr_cycles	- dump the memory in nbr_cycles
+** number		- number of the next players
+** filename		- name of the filename
 */
 
 typedef struct		s_cw_data
 {
 	unsigned int	nbr_cycles;
 	unsigned int	number;
+	char			filename[CW_FILENAME_MAX_LEN];
 }					t_cw_data;
 
 
--- srcs/vm/cw_vm_parsing.c
+++ srcs/vm/cw_vm_parsing.c
@@ -6,66 +6,93 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 14:06:54 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/04 14:06:54 by jthierce         ###   ########.fr       */
+/*   Updated: 2020/06/05 01:35:45 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "cw_vm.h"
 #include "cw_vm_parsing.h"
+#include "cw_errors.h"
+#include "cw_common.h"
 
-int		cw_vm_get_player(char *str, int number)
+
+/*
+** cw_vm_is_valid_extension() checks whether `argv` ends in `extension`
+*/
+
+int		cw_vm_is_valid_extension(const char *argv, const char *extension)
 {
-	int i;
+	if (argv == NULL || extension == NULL)
+		return (CW_FAILURE);
+	if ((extension = ft_strrchr(argv, '.')) != NULL)
+		if (ft_strequ(extension, ".cor"))
+			return (CW_SUCCESS);
+	return (CW_FAILURE);
+}
+
+/*
+** cw_vm_get_player() retrieves player's filename which must have .cor extension
+*/
+
+
+int		cw_vm_get_player(t_cw_data *data, const char *argv)
+{
+	int		i;
 
 	i = -1;
-	ft_printf("{pink}str = %s\n{}", str);
-	(void)number;
-	if (str == NULL)
-		return (CW_ERROR);
-	while (str[++i] && ft_strchr(str + (i + 1), '.') != NULL)
-		if (ft_isalnum(str[i]) == 0)
-			return (CW_ERROR);
-	if (i == 0 && str[i] != '.')
-		return (CW_ERROR);
-	if (ft_strequ(str + i, ".cor\0") == 0)
-		return (CW_ERROR);
-	ft_printf("SUCCESS\n");
+	if (argv == NULL)
+		ft_printerr("cw_vm_get_player()", "filename is empty");
+	if (ft_strlen(argv) > CW_FILENAME_MAX_LEN)
+		ft_printerr("cw_vm_get_player()", "filename is too large");
+	if (cw_vm_is_valid_extension(argv, ".cor") == CW_FAILURE)
+		ft_printerr("cw_vm_get_player()", "filename has incorrect extension");
+	ft_strcpy(data->filename, argv);
 	return (CW_SUCCESS);
 }
+
+/*
+** cw_vm_get_data() retrieves an integer from given argv and return its value
+*/
+
+int		cw_vm_get_data(const char *argv, char *tmp)
+{
+	int			value;
+
+	value = 0;
+	if (argv)
+		if (ft_atoi32check(&value, argv + ft_strspn(argv, " \t")) != 0 || value <= 0)
+		{
+			ft_printf("{red}ERROR: cw_vm_get_data(){}\n");
+			free(tmp);
+			exit(CW_VM_NO_VALID_ARGUMENT_DUMP);
+		}
+	return (value);
+}
+
+/*
+** cw_vm_parsing() parse received arguments to fill t_cw_data structure
+*/
 
 int		cw_vm_parsing(int argc, char **argv, t_cw_data *data)
 {
 	int			i;
-	int			value;
 	char		*tmp;
 
 	i = -1;
 	while (++i < argc)
 	{
 		tmp = ft_strtrim(argv[i]);
-		//if (ft_strequ(argv[i] + ft_strspn(argv[i], " \t"), "-dump") == 1)
 		if (ft_strequ(tmp, "-dump") == 1)
 		{
-			i++;
-			if (ft_atoi32check(&value, argv[i] + ft_strspn(argv[i], " \t")) != 0 || value < 0)
-			{
-				free(tmp);
-				return (CW_VM_NO_VALID_ARGUMENT_DUMP);
-			}
-			data->nbr_cycles = value;
-			ft_printf("{green}-dump is okay %d\n{}", value);
+			data->nbr_cycles = cw_vm_get_data(argv[++i], tmp);
+			ft_printf("data->nbr_cycles = %d\n", data->nbr_cycles);
 		}
-		if (ft_strequ(tmp, "-n") == 1)
+		else if (ft_strequ(tmp, "-n") == 1)
 		{
-			i++;
-			if (ft_atoi32check(&value, argv[i] + ft_strspn(argv[i], " \t")) != 0 || value <= 0)
-			{
-				free(tmp);
-				return (CW_VM_NO_VALID_ARGUMENT_DUMP);
-			}
-			data->number = value;
-			cw_vm_get_player(argv[i + 1], value);
+			data->number = cw_vm_get_data(argv[++i], tmp);
+			ft_printf("data->number = %d\n", data->number);
+			cw_vm_get_player(data, argv[i + 1]);
 		}
 		free(tmp);
 	}
