4c31c96c7924675a1127b17b02e7de88f852a1f6
Jthierce <jordan.thiercelin@gmail.com>
2020-06-05 14:57:27 +0200

    vn.h

--- includes/cw_vm.h
+++ includes/cw_vm.h
@@ -13,11 +13,16 @@
 #ifndef CW_VM_H
 # define CW_VM_H
 
+# define CW_MAX_PLAYERS			4
+# define CW_FILENAME_MAX_LEN	128
+
 # include <stdlib.h>
 # include "cw_errors.h"
 # include "cw_vm_parsing.h"
+# include "cw_vm_player.h"
 
 int		cw_vm_usage(void);
 int		cw_vm_parsing(int argc, char **argv, t_cw_data *data);
+int		cw_vm_read_player(t_cw_data *data, t_cw_champion players);
 
 #endif
