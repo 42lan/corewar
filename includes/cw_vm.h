f003230a38623417f7bd0445777c03b8c9d5f800
4slan <amalsago@student.42.fr>
2020-06-06 10:50:18 +0200

    add t_cw_vm and prototype parsing functions

--- includes/cw_vm.h
+++ includes/cw_vm.h
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 15:01:47 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/04 21:09:48 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/06 00:38:43 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,25 @@
 # define CW_VM_H
 
 # define CW_MAX_PLAYERS			4
-# define CW_FILENAME_MAX_LEN	128
 
 # include <stdlib.h>
 # include "cw_errors.h"
 # include "cw_vm_parsing.h"
 # include "cw_vm_player.h"
 
-int		cw_vm_usage(void);
-int		cw_vm_parsing(int argc, char **argv, t_cw_data *data);
+typedef struct	s_cw_vm
+{
+	t_cw_player	player[CW_MAX_PLAYERS];
+	t_bool		dump;
+
+}				t_cw_vm;
+
+void	cw_vm_usage(void);
+int		cw_vm_parsing(int argc, char **argv, t_cw_vm *vm, t_cw_data *data);
+int		cw_vm_get_data(char **argv);
+int		cw_vm_get_player(t_cw_data *data, char **argv);
+int		cw_vm_is_valid_extension(const char *argv, const char *extension);
+void	cw_vm_print_data(t_cw_data *data);
 int		cw_vm_read_player(t_cw_data *data, t_cw_player *players);
 
 #endif
