7b30b00de4aecd612b89084fa67ebc88c8f99309
4slan <amalsago@student.42.fr>
2020-06-06 23:58:55 +0200

    feat: define cw_vm_read_champion_exec_code() and cw_vm_read_champion_exec_code()

--- srcs/vm/cw_vm_valid_player.c
+++ srcs/vm/cw_vm_valid_player.c
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/06 15:33:02 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/06 22:51:33 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/06 23:55:10 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ int		cw_vm_read_champion_name(int fd, t_cw_player *player)
 ** the name or comment is not too long
 */
 
-int		cw_vm_read_champion_null(int fd, char *str)
+int		cw_vm_read_champion_null(int fd, char *part)
 {
 	char	str[CW_CHAMPION_NULL];
 
@@ -81,7 +81,7 @@ int		cw_vm_read_champion_null(int fd, char *str)
 	}
 	if (ft_memcmp(str, "\0\0\0\0", CW_CHAMPION_NULL) != 0)
 	{
-		ft_dprintf(2, "{red}%s IS TOO LONG\n{}", str);
+		ft_dprintf(2, "{red}%s IS TOO LONG\n{}", part);
 		return (CW_VN_ERROR_NAME_TOO_LONG);
 	}
 	return (CW_SUCCESS);
@@ -95,6 +95,7 @@ int		cw_vm_read_champion_null(int fd, char *str)
 int		cw_vm_read_exec_code_len(int fd, t_cw_player *player)
 {
 	int				i;
+	int32_t			code_len_int;
 	unsigned char	code_len[4];
 
 	i = -1;
@@ -104,8 +105,8 @@ int		cw_vm_read_exec_code_len(int fd, t_cw_player *player)
 		ft_dprintf(2, "{RED}ERROR READ CHAMPION EXEC CODE LEN\n{}");
 		return (CW_VM_READ_ERROR);
 	}
-	while (++i < CW_EXEC_CODE_LEN)
-		player->champion->code_len += code_len[i];
+	code_len_int = ft_bigendian32_read(code_len);
+	player->champion->code_len = code_len_int;
 	return (CW_SUCCESS);
 }
 
@@ -132,6 +133,58 @@ int		cw_vm_read_champion_comment(int fd, t_cw_player *player)
 		ft_dprintf(2, "{red}ERROR MALLOC FAILED\n{}");
 		return (CW_ERROR_MALLOC_FAILED);
 	}
+	return (CW_SUCCESS);
+}
+
+/*
+** cw_vm_read_champion_exec_code() read the last part of the file which is
+** executable code and check if it overflows
+*/
+
+int		cw_vm_read_champion_exec_code(int fd, t_cw_player *player)
+{
+	int				i;
+	unsigned char	*exec_code;
+
+	i = -1;
+	if ((exec_code = ft_memalloc(sizeof(exec_code) * player->champion->code_len)) == NULL)
+	{
+		ft_dprintf(2, "{red}ERROR MALLOC FAILED\n{}");
+		return (CW_ERROR_MALLOC_FAILED);
+	}
+	if (read(fd, exec_code, player->champion->code_len) != player->champion->code_len)
+	{
+		close(fd);
+		ft_dprintf(2, "{RED}ERROR READ CHAMPION EXEC CODE\n{}");
+		return (CW_VM_READ_ERROR);
+	}
+	player->champion->code = exec_code;
+	if ((read(fd, NULL, 1)) != 0)
+	{
+		close(fd);
+		ft_dprintf(2, "{RED}ERROR READ CHAMPION EXEC CODE OVERFLOW\n{}");
+		return (CW_VM_READ_ERROR);
+	}
+	return (CW_SUCCESS);
+}
+
+/*
+** cw_vm_verify_file_structure() calls other functions to verify the validity
+** of `.cor` file
+*/
+
+int		cw_vm_verify_file_structure(int fd, t_cw_player *player)
+{
+	int		ret;
+
+	if (((ret = cw_vm_read_magic_number(fd)) != CW_SUCCESS)
+		|| ((ret = cw_vm_read_champion_name(fd, player)) != CW_SUCCESS)
+		|| ((ret = cw_vm_read_champion_null(fd, "NAME")) != CW_SUCCESS)
+		|| ((ret = cw_vm_read_exec_code_len(fd, player)) != CW_SUCCESS)
+		|| ((ret = cw_vm_read_champion_comment(fd, player)) != CW_SUCCESS)
+		|| ((ret = cw_vm_read_champion_null(fd, "COMMENT")) != CW_SUCCESS)
+		|| ((ret = cw_vm_read_champion_exec_code(fd, player) != CW_SUCCESS)))
+		return (ret);
 	return (CW_SUCCESS);
 }
 
@@ -165,12 +218,7 @@ int		cw_vm_valid_player(t_cw_data *data, t_cw_player *players)
 			ft_dprintf(2, "{red}champion_create() failed\n{}");
 			exit(CW_VM_ERROR_OPEN_FAILED);
 		}
-		if (((ret = cw_vm_read_magic_number(fd)) != CW_SUCCESS)
-			|| ((ret = cw_vm_read_champion_name(fd, &players[i])) != CW_SUCCESS)
-			|| ((ret = cw_vm_read_champion_null(fd, "NAME")) != CW_SUCCESS)
-			|| ((ret = cw_vm_read_exec_code_len(fd, &players[i])) != CW_SUCCESS)
-			|| ((ret = cw_vm_read_champion_comment(fd, &players[i]) != CW_SUCCESS))
-			|| ((ret = cw_vm_read_champion_null(fd, "COMMENT")) != CW_SUCCESS))
+		if ((ret = cw_vm_verify_file_structure(fd, &players[i])) != CW_SUCCESS)
 		{
 			while (i != -1)
 				cw_champion_destroy(&players[i--].champion);
