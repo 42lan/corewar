2c16102395c75f2ea15b9666560bc1040a37142a
4slan <amalsago@student.42.fr>
2020-06-07 01:53:02 +0200

    del: remove call to debug

--- srcs/vm/cw_vm_read_player.c
+++ srcs/vm/cw_vm_read_player.c
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/05 13:20:57 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/06 00:43:44 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/07 01:35:28 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "cw_vm_player.h"
 
 /*
-** Open file .cor and take all information to place in structure players
+** Open `.cor` file and take all information to fill player's structure
 ** Return error code when the file is not validate
 */
 
@@ -28,7 +28,6 @@ int			cw_vm_read_player(t_cw_data *data, t_cw_player *players)
 		exit(CW_ERROR_MALLOC_FAILED);
 	}
 	cw_parsing_number_player(data);
-	cw_vm_print_data(data);	//debug function
 	cw_vm_valid_player(data, players);
 	return (CW_SUCCESS);
 }
