b8c54f2ff516479f2b4a89e00961ba64eca52db8
4slan <amalsago@student.42.fr>
2020-06-06 10:40:58 +0200

    give explicit name

--- includes/cw_vm_parsing.h
+++ includes/cw_vm_parsing.h
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 18:48:57 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/05 01:29:01 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/06 10:39:13 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 ** Data struct create with parsing
 **
 ** nbr_cycles	- dump the memory in nbr_cycles
-** number		- number of the next players
+** nbr_players	- number of players
 ** filename		- name of the filename
 ** filenumber	- id of players (-1 is a default value)
 */
@@ -27,7 +27,7 @@
 typedef struct		s_cw_data
 {
 	int				nbr_cycles;
-	int				number;
+	int				nbr_players;
 	char			*filename[CW_MAX_PLAYERS];
 	int				filenumber[CW_MAX_PLAYERS];
 }					t_cw_data;
