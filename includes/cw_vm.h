7fc0e16d72944b31bedbe1343d141a6c6b0b7f3f
4slan <amalsago@student.42.fr>
2020-06-06 15:54:48 +0200

    fix & feat: pass vm struct and define new f()

--- includes/cw_vm.h
+++ includes/cw_vm.h
@@ -29,11 +29,12 @@ typedef struct	s_cw_vm
 }				t_cw_vm;
 
 void	cw_vm_usage(void);
-int		cw_vm_parsing(int argc, char **argv, t_cw_vm *vm, t_cw_data *data);
+int		cw_vm_parsing(int argc, char **argv, t_cw_vm *vm);
 int		cw_vm_get_data(char **argv);
-int		cw_vm_get_player(t_cw_data *data, char **argv);
+int		cw_vm_get_player(t_cw_vm *vm, char *filename);
 int		cw_vm_is_valid_extension(const char *argv, const char *extension);
 void	cw_vm_print_data(t_cw_data *data);
 int		cw_vm_read_player(t_cw_data *data, t_cw_player *players);
+void	cw_vm_check_nbr_players(t_cw_vm *vm);
 
 #endif
