6f69db4030ddb4b4beb2bdb1cd9d336295260921
4slan <amalsago@student.42.fr>
2020-06-07 14:15:13 +0200

    style: print only address and hex values

--- srcs/vm/cw_vm_arena_dump.c
+++ srcs/vm/cw_vm_arena_dump.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/07 12:41:56 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/07 12:42:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/07 14:14:04 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,17 @@ void	cw_vm_arena_dump(unsigned char *arena, unsigned int size)
 	{
 		if ((i % 64) == 0)
 		{
-			(i != 0) ? ft_printf(" %s\n", buff) : 0;
+			(i != 0) ? ft_putchar('\n') : 0;
 			ft_printf("{bold}0x%06x:{}", i);
 		}
-		(i % 2 == 0) ? ft_printf(" ") : 0;
+		(i % 2 == 0) ? ft_putchar(' ') : 0;
 		ft_printf("%02x", pc[i]);
 		buff[(i % 64) + 1] = '\0';
 	}
 	while ((i % 64) != 0)
 	{
-		(i % 2 == 0) ? ft_printf(" ") : 0;
+		(i % 2 == 0) ? ft_putchar(' ') : 0;
 		ft_printf("00", i++);
 	}
-	ft_printf("\n");
+	ft_putchar('\n');
 }
