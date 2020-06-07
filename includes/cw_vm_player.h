da4f0730b5726232cb4d6e55f7c3eb67a9c80aad
4slan <amalsago@student.42.fr>
2020-06-07 02:10:15 +0200

    feat: add protypes of read file functions

--- includes/cw_vm_player.h
+++ includes/cw_vm_player.h
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 14:38:41 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/06 10:41:58 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/07 02:06:16 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,11 @@ typedef struct		s_cw_player
 
 void	cw_parsing_number_player(t_cw_data *data);
 int		cw_vm_valid_player(t_cw_data *data, t_cw_player *players);
+int		cw_vm_read_magic_number(int fd);
+int		cw_vm_read_champion_name(int fd, t_cw_player *player);
+int		cw_vm_read_champion_null(int fd, char *part);
+int		cw_vm_read_exec_code_len(int fd, t_cw_player *player);
+int		cw_vm_read_champion_comment(int fd, t_cw_player *player);
+int		cw_vm_read_champion_exec_code(int fd, t_cw_player *player);
 
 #endif
