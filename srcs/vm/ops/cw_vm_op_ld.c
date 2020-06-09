a57fb3bbca604f4e3256bb82117886f6c5fe205f
Jthierce <jordan.thiercelin@gmail.com>
2020-06-09 19:00:35 +0200

    ld juste left error gestion

--- srcs/vm/ops/cw_vm_op_ld.c
+++ srcs/vm/ops/cw_vm_op_ld.c
@@ -10,7 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cw_operations.h"
+#include "libft.h"
+#include "cw_vm_battle.h"
+#include "cw_inst.h"
 
 void	cw_vm_op_ld_dir_exec(t_cw_battle *battle, t_cw_vm *vm)
 {
@@ -21,23 +23,17 @@ void	cw_vm_op_ld_dir_exec(t_cw_battle *battle, t_cw_vm *vm)
 
 	i = -1;
 	arg1 = 0;
-	multiplier = 256 * 256 * 256;
+	multiplier = ft_pow(256, CW_DIR_SIZE_LD - 1);
 	while (++i < CW_DIR_SIZE_LD)
 	{
 		arg1 += multiplier * vm->arena[battle->processus->position + 2 + i];
 		multiplier /= 256;
 	}
-	ft_printf("{red}ARG1    %ld\n", arg1);
-
-	// Write the number from the first argument into the registry received as second argument
 	index = vm->arena[battle->processus->position + 2 + i];
-	battle->processus->registries[index] = arg1;
-	ft_printf("reg[%d]  %ld\n", index, battle->processus->registries[index]);
-
-	// If the written value is 0, then set carry to 1; if the written vaue is !0 then set carry to 0
-	battle->processus->carry = (arg1 == 0) ? 0 : 1;
-	ft_printf("carry   %d\n{}", battle->processus->carry);
-	exit(1);
+	if (index < 1 || index > CW_REG_NUMBER)
+		ft_printf("ERROR\n");
+	battle->processus->registries[index - 1] = arg1;
+	battle->processus->carry = (arg1 == 0) ? 1 : 0;
 }
 
 void	cw_vm_op_ld_ind_exec(t_cw_battle *battle, t_cw_vm *vm)
@@ -45,29 +41,29 @@ void	cw_vm_op_ld_ind_exec(t_cw_battle *battle, t_cw_vm *vm)
 	int				i;
 	int				index;
 	unsigned int	arg1;
-	unsigned int	arg2;
 	unsigned int	multiplier;
 	int				idx_address;
 
-	ft_printf("IDIRD\n");
 	i = -1;
 	arg1 = 0;
-	multiplier = 256 * 256 * 256;
-	while (++i < CW_DIR_SIZE_LD)
+	multiplier = 256;
+	while (++i < 2)
 	{
 		arg1 += multiplier * vm->arena[battle->processus->position + 2 + i];
 		multiplier /= 256;
 	}
-	ft_printf("{red}ARG1    %ld\n", arg1);
-	// Calculate address: current position of the cursor + <FIRST_ARGUMENT> % IDX_MOD.
-	idx_address = vm->arena[battle->processus->position] + (arg1 % CW_IDX_MOD);
-	// Read four bytes starting from the obtained address.
-	// Write the value from the step 2 into the registry passed as the second argument.
+	idx_address = battle->processus->position + (arg1 % CW_IDX_MOD);
 	index = vm->arena[battle->processus->position + 2 + i];
-	battle->processus->registries[index] = ;
-	// If the written value is 0, then set carry to 1; if the written vaue is !0 then set carry to 0.
-	battle->processus->carry = (battle->processus->registries[index] == 0) ? 1 : 0;
-
+	i = -1;
+	arg1 = 0;
+	multiplier = ft_pow(256, 3);
+	while (++i < 4)
+	{
+		arg1 += multiplier * vm->arena[idx_address + i];
+		multiplier /= 256;
+	}
+	battle->processus->registries[index - 1] = arg1;
+	battle->processus->carry = (arg1 == 0) ? 1 : 0;
 }
 
 void	cw_vm_op_ld(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
@@ -80,4 +76,5 @@ void	cw_vm_op_ld(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 		cw_vm_op_ld_dir_exec(battle, vm);
 	else if (inst->types[0] == T_IND)
 		cw_vm_op_ld_ind_exec(battle, vm);
+	exit(1);
 }
