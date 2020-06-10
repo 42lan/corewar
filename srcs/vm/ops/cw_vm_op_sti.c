1392df27b9536ae8b21f6df13294d2cca798cbba
Jthierce <jordan.thiercelin@gmail.com>
2020-06-11 01:20:02 +0200

    op_sti

--- srcs/vm/ops/cw_vm_op_sti.c
+++ srcs/vm/ops/cw_vm_op_sti.c
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 17:13:22 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/10 22:48:59 by jthierce         ###   ########.fr       */
+/*   Updated: 2020/06/11 01:04:53 by jthierce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,104 @@
 #include "cw_vm_battle.h"
 #include "cw_inst.h"
 
+int		cw_vm_op_sti_dir(t_cw_battle *battle, t_cw_vm *vm, int pos)
+{
+	int total;
+	int i;
+	int pow;
+
+	i = -1;
+	total = 0;
+	pow = ft_pow(256, CW_DIR_SIZE_STI - 1);
+	while (++i < CW_DIR_SIZE_STI)
+	{
+		total += pow * vm->arena[(battle->processus->position + pos + i) % CW_MEM_SIZE];
+		pow /= 256;
+	}
+	return (total);
+}
+
+int16_t	cw_vm_op_sti_ind(t_cw_battle *battle, t_cw_vm *vm, int pos)
+{
+	int				i;
+	int16_t			arg;
+	int				total;
+	unsigned int	multiplier;
+	int				idx_address;
+
+	i = -1;
+	arg = 0;
+	total = 0;
+	multiplier = 256;
+	while (++i < 2)
+	{
+		arg += multiplier * vm->arena[(battle->processus->position + pos + i) % CW_MEM_SIZE];
+		multiplier /= 256;
+	}
+	idx_address = (battle->processus->position + (arg % CW_IDX_MOD)) % CW_MEM_SIZE;
+	if (idx_address < 0)
+		idx_address += CW_MEM_SIZE;
+	i = -1;
+	arg = 0;
+	multiplier = ft_pow(256, CW_DIR_SIZE_STI - 1);
+	while (++i < CW_DIR_SIZE_STI)
+	{
+		total += multiplier * vm->arena[(idx_address + i) % CW_MEM_SIZE];
+		multiplier /= 256;
+	}
+	return (arg);
+}
+
+void	cw_vm_op_sti_body(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
+{
+	int i;
+	int idx_address;
+	int	arg[3];
+	int	pos;
+	int reg_value;
+	int pow;
+
+	i = 0;
+	pos = 3;
+	reg_value = 0;
+	while (++i < 3)
+	{
+		if (inst->args[i] == T_REG)
+		{
+			if ((vm->arena[battle->processus->position + pos]) < 1 || (vm->arena[battle->processus->position + pos]) > 16)
+				ft_printf("ERROR\n");
+			arg[i] = battle->processus->registries[(vm->arena[battle->processus->position + pos]) - 1];
+			pos++;
+		}
+		else if (inst->args[i] == T_DIR)
+		{
+			arg[i] = cw_vm_op_sti_dir(battle, vm, pos);
+			pos += CW_DIR_SIZE_STI;
+		}
+		else if (inst->args[i] == T_IND)
+		{
+			arg[i] = cw_vm_op_sti_ind(battle, vm, pos);
+			pos += 2;
+		}
+	}
+	idx_address = battle->processus->position + (arg[1] + arg[2]) % CW_IDX_MOD;
+	pow = ft_pow(256, 3);
+	i = -1;
+	reg_value = battle->processus->registries[battle->processus->position + 1];
+	while (++i < 4)
+	{
+		vm->arena[(idx_address + i) % CW_MEM_SIZE] = reg_value / pow;
+		reg_value %= pow;
+		pow /= 256;
+	}
+	battle->processus->position = pos;
+}
+
 void	cw_vm_op_sti(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 {
-	if (inst->args_count != 2)
+	if (inst->args_count != 3)
 		ft_printf("ERROR\n");
-	if (inst->types[0] != T_REG || inst->types[1] == T_DIR)	
+	if (inst->types[0] != T_REG || inst->types[2] == T_IND)	
 		ft_printf("ERROR\n");
-	if (inst->types[1] == T_REG)
-	{
-		battle->processus->registries[vm->arena[(battle->processus->position + 3) % CW_MEM_SIZE] - 1] =
-		battle->processus->registries[vm->arena[(battle->processus->position + 2) % CW_MEM_SIZE] - 1];
-		battle->processus->position += 4;
-	}
-		// rajouter une protection si la valeur de l'index est superieur a 16 ou < 1
-	//else if (inst->types[1] == T_IND)
-	//	cw_vm_op_st_ind_exec(battle, vm);
-	cw_vm_arena_dump(vm->arena, CW_MEM_SIZE);
+	cw_vm_op_sti_body(inst, battle, vm);
 }
