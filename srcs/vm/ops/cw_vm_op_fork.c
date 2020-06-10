19c257ac81c65b869428b2d3cb92881c91830842
4slan <amalsago@student.42.fr>
2020-06-10 22:09:38 +0200

    feat: add malloc part to op_fork

--- srcs/vm/ops/cw_vm_op_fork.c
+++ srcs/vm/ops/cw_vm_op_fork.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 17:13:59 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/10 19:58:32 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/10 22:08:06 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@
 
 void	cw_vm_op_fork(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 {
-	int			i;
-	int16_t		arg1;
-	int			idx_address;
-	int			multiplier;
+	int				i;
+	int16_t			arg1;
+	int				idx_address;
+	int				multiplier;
+	t_cw_processus	*new;
 
 	i = -1;
 	arg1 = 0;
 	multiplier = ft_pow(256, CW_DIR_SIZE_FORK - 1);
+	ft_printf("%d %d\n", inst->args_count, inst->types[0]);
 	if (inst->args_count != 1 || inst->types[0] != T_DIR)
 		ft_printf("ERROR\n");
 	while (++i < CW_DIR_SIZE_FORK)
@@ -31,8 +33,13 @@ void	cw_vm_op_fork(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 		arg1 += multiplier * vm->arena[(battle->processus->position + 1 + i) % CW_MEM_SIZE];
 		multiplier /= 256;
 	}
-	idx_address = arg1 % CW_IDX_MOD;
-	// alloc
+	idx_address = battle->processus->position + arg1 % CW_IDX_MOD;
+	if (!((new = ft_memdup(battle->processus, sizeof(t_cw_processus *)))))
+		exit(CW_ERROR_MALLOC_FAILED);
+	new->position = idx_address;
+	new->carry = false;
+	new->last_live = -1;
+	new->wait_cycles = 0;
 	battle->processus->position += 3 % CW_MEM_SIZE;
 	exit(0);
 }
