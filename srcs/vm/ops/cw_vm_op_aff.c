048e87f7e5d3df9662f2189528a6dd0c2d3eccec
4slan <amalsago@student.42.fr>
2020-06-10 18:22:17 +0200

    feat: define op_aff

--- srcs/vm/ops/cw_vm_op_aff.c
+++ srcs/vm/ops/cw_vm_op_aff.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 17:15:23 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/10 17:15:28 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/10 18:22:02 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,15 @@
 
 void	cw_vm_op_aff(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 {
-	(void)inst;(void)battle;(void)vm;
+	int		arg1;
+	int		index;
+	char	reg_value;
+
+	if (inst->args_count != 1 || inst->types[0] != T_REG)
+		ft_printf("ERROR\n");
+	index = vm->arena[battle->processus->position + 2] - 1;
+	arg1 = battle->processus->registries[index];
+	reg_value = arg1 % 256;
+	ft_printf("{bold}{red}%c{}\n", reg_value);
+	battle->processus->position += 3 % CW_MEM_SIZE;
 }
