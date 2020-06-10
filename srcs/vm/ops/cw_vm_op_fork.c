9db773aff9c32013f45187dcddd509711b5a9840
4slan <amalsago@student.42.fr>
2020-06-10 19:59:13 +0200

    feat: define fork (still malloc)

--- srcs/vm/ops/cw_vm_op_fork.c
+++ srcs/vm/ops/cw_vm_op_fork.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 17:13:59 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/10 17:14:15 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/10 19:58:32 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,23 @@
 
 void	cw_vm_op_fork(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 {
-	(void)inst;(void)battle;(void)vm;
+	int			i;
+	int16_t		arg1;
+	int			idx_address;
+	int			multiplier;
+
+	i = -1;
+	arg1 = 0;
+	multiplier = ft_pow(256, CW_DIR_SIZE_FORK - 1);
+	if (inst->args_count != 1 || inst->types[0] != T_DIR)
+		ft_printf("ERROR\n");
+	while (++i < CW_DIR_SIZE_FORK)
+	{
+		arg1 += multiplier * vm->arena[(battle->processus->position + 1 + i) % CW_MEM_SIZE];
+		multiplier /= 256;
+	}
+	idx_address = arg1 % CW_IDX_MOD;
+	// alloc
+	battle->processus->position += 3 % CW_MEM_SIZE;
+	exit(0);
 }
