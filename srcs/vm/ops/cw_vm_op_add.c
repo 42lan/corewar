e660dccf465278cacd51ce39674d67929b6da85c
4slan <amalsago@student.42.fr>
2020-06-10 17:50:34 +0200

    refact: use array of ints to reduce number of variables

--- srcs/vm/ops/cw_vm_op_add.c
+++ srcs/vm/ops/cw_vm_op_add.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 00:04:37 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/10 01:09:09 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/10 17:47:12 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,14 @@
 
 void	cw_vm_op_add(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 {
-	char	arg1;
-	char	arg2;
-	char	arg3;
+	int		arg[3];
 
 	if (inst->args_count != 3 || inst->types[0] != T_REG || inst->types[1] == T_REG || inst->types[2] != T_REG)
 		ft_printf("ERROR\n");
-	arg1 = battle->processus->registries[vm->arena[(battle->processus->position + 2) % CW_MEM_SIZE] - 1];
-	arg2 = battle->processus->registries[vm->arena[(battle->processus->position + 3) % CW_MEM_SIZE] - 1];
-	arg3 = arg1 + arg2;
-	battle->processus->registries[vm->arena[(battle->processus->position + 4) % CW_MEM_SIZE] - 1] = arg3;
-	battle->processus->carry = (arg3 == 0) ? 1 : 0;
+	arg[0] = battle->processus->registries[vm->arena[(battle->processus->position + 2) % CW_MEM_SIZE] - 1];
+	arg[1] = battle->processus->registries[vm->arena[(battle->processus->position + 3) % CW_MEM_SIZE] - 1];
+	arg[2] = arg[0] + arg[1];
+	battle->processus->registries[vm->arena[(battle->processus->position + 4) % CW_MEM_SIZE] - 1] = arg[2];
+	battle->processus->carry = (arg[2] == 0) ? 1 : 0;
 	battle->processus->position = (battle->processus->position + 2) % CW_MEM_SIZE;
 }
--- srcs/vm/ops/cw_vm_op_sub.c
+++ srcs/vm/ops/cw_vm_op_sub.c
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 00:01:49 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/10 00:01:49 by jthierce         ###   ########.fr       */
+/*   Updated: 2020/06/10 17:48:17 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,16 @@
 
  void	cw_vm_op_sub(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 {
-	int		arg1;
-	int		arg2;
-	int		arg3;
+	int		arg[3];
 
 	if (inst->args_count != 3)
 		ft_printf("ERROR\n");
 	if (inst->types[0] != T_REG || inst->types[1] != T_REG || inst->types[2] != T_REG)	
 		ft_printf("ERROR\n");
-	arg1 = battle->processus->registries[vm->arena[(battle->processus->position + 2) % CW_MEM_SIZE] - 1];
-	arg2 = battle->processus->registries[vm->arena[(battle->processus->position + 3) % CW_MEM_SIZE] - 1];
-	arg3 = arg1 - arg2;
-	battle->processus->registries[vm->arena[(battle->processus->position + 4) % CW_MEM_SIZE] - 1] = arg3;
-	battle->processus->carry = (arg3 == 0) ? 1 : 0;
+	arg[0] = battle->processus->registries[vm->arena[(battle->processus->position + 2) % CW_MEM_SIZE] - 1];
+	arg[1] = battle->processus->registries[vm->arena[(battle->processus->position + 3) % CW_MEM_SIZE] - 1];
+	arg[2] = arg[0] - arg[1];
+	battle->processus->registries[vm->arena[(battle->processus->position + 4) % CW_MEM_SIZE] - 1] = arg[2];
+	battle->processus->carry = (arg[2] == 0) ? 1 : 0;
 	battle->processus->position = (battle->processus->position + 5) % CW_MEM_SIZE;
 }
