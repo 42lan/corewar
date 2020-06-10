027ad48183c165ccf14f9c037b4dc1a2894783c2
Jthierce <jordan.thiercelin@gmail.com>
2020-06-10 23:10:47 +0200

    lfork

--- srcs/vm/ops/cw_vm_op_lfork.c
+++ srcs/vm/ops/cw_vm_op_lfork.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cw_vm_op_lfork.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 17:15:10 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/10 17:15:14 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/10 23:10:07 by jthierce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,26 @@
 
 void	cw_vm_op_lfork(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 {
-	(void)inst;(void)battle;(void)vm;
+	int				i;
+	int16_t			arg1;
+	int				idx_address;
+	int				multiplier;
+	t_cw_processus	*new;
+
+	i = -1;
+	arg1 = 0;
+	multiplier = ft_pow(256, CW_DIR_SIZE_LFORK - 1);
+	(void)inst;
+	while (++i < CW_DIR_SIZE_LFORK)
+	{
+		arg1 += multiplier * vm->arena[(battle->processus->position + 1 + i) % CW_MEM_SIZE];
+		multiplier /= 256;
+	}
+	idx_address = battle->processus->position + arg1;
+	if (!(new = ft_memdup(battle->processus, sizeof(t_cw_processus)))) //a verifier le tableau de registre
+		exit(CW_ERROR_MALLOC_FAILED);
+	new->position = idx_address;
+	new->next = battle->head;
+	battle->head = new;
+	battle->processus->position = (battle->processus->position + 3) % CW_MEM_SIZE;
 }
