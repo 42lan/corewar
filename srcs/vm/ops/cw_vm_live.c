2ee42a266d60c2ecfd678b767947ca81f1d07328
4slan <amalsago@student.42.fr>
2020-06-11 15:58:58 +0200

    fix: get arg1 correctrly

--- srcs/vm/ops/cw_vm_live.c
+++ srcs/vm/ops/cw_vm_live.c
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 16:59:44 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/10 17:10:47 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/11 15:33:08 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,17 @@ void	cw_vm_op_live(t_cw_inst *inst, t_cw_battle *battle, t_cw_vm *vm)
 
 	i = -1;
 	arg1 = 0;
-	multiplier = ft_pow(256, CW_DIR_SIZE_LD - 1);
+	multiplier = ft_pow(256, CW_DIR_SIZE_LIVE - 1);
 	(void)inst;
 	while (++i < CW_DIR_SIZE_LIVE)
 	{
-		arg1 += multiplier * vm->arena[(battle->procs->pos + 2 + i) % CW_MEM_SIZE];
+		arg1 += multiplier * vm->arena[(battle->procs->pos + 1 + i) % CW_MEM_SIZE];
 		multiplier /= 256;
 	}
-	ft_printf(">>%ld\n", arg1);
+	ft_printf(">>>>>%d\n", arg1);
+	if ((unsigned)arg1 <= vm->players->number)
+		ft_printf("---\n");
+
+	exit(0);
+	/* ft_printf("Player %d (%s) is alive.\n", ); */
 }
