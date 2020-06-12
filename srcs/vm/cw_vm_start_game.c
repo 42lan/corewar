df2639c8742b036206aaa4356e28c0ab61218e30
4slan <amalsago@student.42.fr>
2020-06-12 15:42:35 +0200

    style: refact function to respect the norm

--- srcs/vm/cw_vm_start_game.c
+++ srcs/vm/cw_vm_start_game.c
@@ -6,16 +6,29 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/07 19:14:23 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/08 17:52:10 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/12 15:36:37 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cw_vm_battle.h"
 #include "cw_vm.h"
 
-void	cw_vm_processus_dead(t_cw_battle *battle, t_cw_proc *proc, t_cw_vm *vm)
+static int		cw_vm_last_proc(t_cw_battle *battle, t_cw_vm *vm)
 {
-	t_cw_proc *prev;
+	int			id;
+	char		*name;
+
+	id = battle->last_alive;
+	name = vm->players[battle->last_alive].champion->name;
+	ft_printf("{bold}{orange}The player %d(%s) has won.{}\n", id, name);
+	(vm->dump == true) ? cw_vm_arena_dump(vm->arena, CW_MEM_SIZE) : 0;
+	return (CW_VM_LAST_PROC);
+
+}
+
+static int		cw_vm_processus_dead(t_cw_battle *battle, t_cw_proc *proc, t_cw_vm *vm)
+{
+	t_cw_proc	*prev;
 
 	while (proc != NULL)
 	{
@@ -24,18 +37,12 @@ void	cw_vm_processus_dead(t_cw_battle *battle, t_cw_proc *proc, t_cw_vm *vm)
 			if (proc == battle->head)
 			{
 				if (proc->next == NULL)
-				{
-					ft_printf("{bold}{red}FIN DU MATCH\n{}");
-					ft_printf("{yellow}ET LE GRAND GAGNANT EST %d\n", battle->last_alive);
-					cw_vm_arena_dump(vm->arena, CW_MEM_SIZE);
-					exit (1);
-				}
+					return (cw_vm_last_proc(battle, vm));
 				battle->head = proc->next;
 			}
 			else
 				prev->next = proc->next;
-			free(proc);
-			proc = NULL;
+			ft_memdel((void **)&proc);
 		}
 		else
 		{
@@ -44,6 +51,28 @@ void	cw_vm_processus_dead(t_cw_battle *battle, t_cw_proc *proc, t_cw_vm *vm)
 			proc = proc->next;
 		}
 	}
+	return (CW_VM_NOT_LAST_PROC);
+}
+
+static int		cw_vm_perform_check(t_cw_battle *battle, t_cw_vm *vm)
+{
+	battle->check_performed++;
+	battle->cycles_count++;
+	if (battle->cycles_count >= battle->cycle_to_die)
+	{
+		battle->check_performed++;
+		if (cw_vm_processus_dead(battle, battle->head, vm) == CW_VM_LAST_PROC)
+			return (CW_VM_LAST_PROC);
+		if (battle->check_performed >= CW_MAX_CHECKS || battle->count_last_live >= CW_NBR_LIVE)
+		{
+			battle->cycle_to_die -= CW_CYCLE_DELTA;
+			battle->check_performed = 0;
+		}
+		battle->count_last_live = 0;
+		battle->cycles_count = 0;
+	}
+	battle->procs = battle->head;
+	return (CW_VM_NOT_LAST_PROC);
 }
 
 void	cw_vm_start_game(t_cw_battle *battle, t_cw_vm *vm)
@@ -52,15 +81,11 @@ void	cw_vm_start_game(t_cw_battle *battle, t_cw_vm *vm)
 	{
 		if (battle->procs->wait_cycles != 0)
 		{
-			//ft_printf("{green}%d\n{}", battle->procs->wait_cycles);
 			battle->procs->wait_cycles--;
 			if (battle->procs->wait_cycles == 0)
-			{
 				cw_vm_read_execute(battle, vm);
-			}
 		}
-		else if (vm->arena[battle->procs->pos] < 17
-				&& vm->arena[battle->procs->pos] > 0)
+		else if (cw_vm_is_valid_op(vm->arena[battle->procs->pos]) == true)
 		{
 			battle->procs->opc = vm->arena[battle->procs->pos];
 			battle->procs->wait_cycles = battle->cycle_opc[battle->procs->opc - 1];
@@ -70,22 +95,7 @@ void	cw_vm_start_game(t_cw_battle *battle, t_cw_vm *vm)
 		if (battle->procs->next != NULL)
 			battle->procs = battle->procs->next;
 		else
-		{
-			battle->check_performed++;
-			battle->cycles_count++;
-			if (battle->cycles_count >= battle->cycle_to_die)
-			{
-				battle->check_performed++;
-				cw_vm_processus_dead(battle, battle->head, vm);
-				if (battle->check_performed >= CW_MAX_CHECKS || battle->count_last_live >= CW_NBR_LIVE)
-				{
-					battle->cycle_to_die -= CW_CYCLE_DELTA;
-					battle->check_performed = 0;
-				}
-				battle->count_last_live = 0;
-				battle->cycles_count = 0;
-			}
-			battle->procs = battle->head;
-		}
+			if (cw_vm_perform_check(battle, vm) == CW_VM_LAST_PROC)
+				break ;
 	}
 }
