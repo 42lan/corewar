0fe591dbf57a4d1bc4cb5c5c6b0ada1948b5e95e
4slan <amalsago@student.42.fr>
2020-06-14 22:34:30 +0200

    chore: champ with invalid encoding byte

---
+++ champ_false/and/and_base_direct.s
@@ -0,0 +1,7 @@
+.name	"and_basic_dir_pos"
+.comment	"basic use add with reg argument"
+
+	and	%13284265, %372918497, r16
+	and	-7, %24194721, r12
+	st	r16, 20
+	st	r12, 50
---
+++ champ_false/st/st_base_indir.s
@@ -0,0 +1,5 @@
+.name	"st_basic_indir_pos"
+.comment	"basic use st with indir argument"
+
+	ld	%-273, r16
+	st	r16, -25
