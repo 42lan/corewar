c2fb83d98138ab9d8a62294630f78eacb9d4d2ff
4slan <amalsago@student.42.fr>
2020-06-14 16:27:27 +0200

    refact: use bigendian functions() to get args

--- srcs/vm/ops/cw_vm_fork.c
+++ srcs/vm/ops/cw_vm_fork.c
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 17:13:59 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/13 21:09:04 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/14 16:05:59 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,21 @@
 
 void			cw_vm_op_fork(t_cw_inst *inst, t_cw_game *game, t_cw_vm *vm)
 {
-	int			i;
 	int			idx_address;
-	int			mult;
 	int16_t		arg1;
 	t_cw_proc	*new;
 
-	if (inst->args_count == 1 && inst->types[0] == T_DIR)
-	{
-		i = -1;
-		arg1 = 0;
-		mult = ft_pow(256, CW_DIR_SIZE_FORK - 1);
-		while (++i < CW_DIR_SIZE_FORK)
-		{
-			arg1 += mult * vm->arena[(game->procs->pos + 1 + i) % CW_MEM_SIZE];
-			mult /= 256;
-		}
-		idx_address = game->procs->pos + (arg1 % CW_IDX_MOD);
-		if (!(new = ft_memdup(game->procs, sizeof(t_cw_proc)))) //a verifier le tableau de registre
-			exit(CW_ERROR_MALLOC_FAILED);
-		new->pos = idx_address;
-		new->next = game->head;
-		game->head = new;
-	}
+	(void)inst;
+	arg1 = ft_bigendian16_read(vm->arena + ((game->procs->pos + 1) % CW_MEM_SIZE));
+	idx_address = game->procs->pos + (arg1 % CW_IDX_MOD);
+	if (!(new = ft_memdup(game->procs, sizeof(t_cw_proc)))) //a verifier le tableau de registre
+		exit(CW_ERROR_MALLOC_FAILED);
+	// COPIER LE TABLEAU DE REGS[]
+	new->pos = idx_address;
+	new->next = game->head;
+	game->head = new;
 	// opc + T_DIR
 	// 3
-	game->procs->pos += (1 + ((inst->types[0] == T_DIR) ? CW_DIR_SIZE_FORK : 0)) % CW_MEM_SIZE;
+	game->procs->pos = (game->procs->pos + 1 + CW_DIR_SIZE_FORK) % CW_MEM_SIZE;
 	/* ft_printf("0x%02x 0x%02x 0x%02x\n", vm->arena[game->procs->pos-1], vm->arena[game->procs->pos], vm->arena[game->procs->pos + 1]); */
 }
--- srcs/vm/ops/cw_vm_lfork.c
+++ srcs/vm/ops/cw_vm_lfork.c
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 17:15:10 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/13 21:09:41 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/14 16:18:35 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,21 @@
 
 void			cw_vm_op_lfork(t_cw_inst *inst, t_cw_game *game, t_cw_vm *vm)
 {
-	int			i;
-	int16_t		arg1;
 	int			idx_address;
-	int			mult;
+	int16_t		arg1;
 	t_cw_proc	*new;
 
-	i = -1;
-	arg1 = 0;
-	mult = ft_pow(256, CW_DIR_SIZE_LFORK - 1);
-	if (inst->args_count == 1 && inst->types[0] == T_DIR)
-	{
-		while (++i < CW_DIR_SIZE_LFORK)
-		{
-			arg1 += mult * vm->arena[(game->procs->pos + 1 + i) % CW_MEM_SIZE];
-			mult /= 256;
-		}
-		idx_address = game->procs->pos + arg1;
-		if (!(new = ft_memdup(game->procs, sizeof(t_cw_proc)))) //a verifier le tableau de registre
-			exit(CW_ERROR_MALLOC_FAILED);
-		new->pos = idx_address;
-		new->next = game->head;
-		game->head = new;
-	}
+	(void)inst;
+	arg1 = ft_bigendian16_read(vm->arena + ((game->procs->pos + 1) % CW_MEM_SIZE));
+	idx_address = game->procs->pos + arg1;
+	if (!(new = ft_memdup(game->procs, sizeof(t_cw_proc)))) //a verifier le tableau de registre
+		exit(CW_ERROR_MALLOC_FAILED);
+	// COPIER LE TABLEAU DES REGS[]
+	new->pos = idx_address;
+	new->next = game->head;
+	game->head = new;
 	// opc + T_DIR
 	// 3
-	// 1
-	game->procs->pos += (1 + ((inst->types[0] == T_DIR) ? CW_DIR_SIZE_LFORK : 0)) % CW_MEM_SIZE;
+	game->procs->pos = (game->procs->pos + 1 + CW_DIR_SIZE_LFORK) % CW_MEM_SIZE;
 	/* ft_printf("0x%02x 0x%02x 0x%02x\n", vm->arena[game->procs->pos-1], vm->arena[game->procs->pos], vm->arena[game->procs->pos + 1]); */
 }
