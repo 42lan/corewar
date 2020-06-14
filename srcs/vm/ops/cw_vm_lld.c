8f98e3354d6e2ec72598fda5879cf1751bbd4718
Jthierce <jordan.thiercelin@gmail.com>
2020-06-14 20:21:31 +0200

    fin des upgrades

--- srcs/vm/ops/cw_vm_lld.c
+++ srcs/vm/ops/cw_vm_lld.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cw_vm_lld.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 17:14:37 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/13 21:03:20 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/14 19:51:19 by jthierce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,58 +16,41 @@
 
 void				cw_vm_op_lld_dir_exec(t_cw_game *game, t_cw_vm *vm)
 {
-	int				i;
-	int32_t			arg1;
 	int				index;
-	unsigned int	mult;
+	int32_t			arg1;
 
-	i = -1;
-	arg1 = 0;
-	mult = ft_pow(256, CW_DIR_SIZE_LLD - 1);
-	while (++i < CW_DIR_SIZE_LLD)
+	arg1 = ft_bigendian32_read(vm->arena + ((game->procs->pos + 2) % CW_MEM_SIZE));
+	index = vm->arena[(game->procs->pos + 6) % CW_MEM_SIZE];
+	if (cw_vm_is_reg(index) == true)
 	{
-		arg1 += mult * vm->arena[(game->procs->pos + 2 + i) % CW_MEM_SIZE];
-		mult /= 256;
+		game->procs->regs[index - 1] = arg1;
+		game->procs->carry = (arg1 == 0) ? 1 : 0;
 	}
-	index = vm->arena[(game->procs->pos + 2 + i) % CW_MEM_SIZE] - 1;
-	(cw_vm_is_reg(index) == false) ? ft_printf("ERROR\n") : 0;
-	game->procs->regs[index] = arg1;
 }
 
 void				cw_vm_op_lld_ind_exec(t_cw_game *game, t_cw_vm *vm)
 {
-	int				i;
-	int16_t			arg1;
-	unsigned int	mult;
-	int				to_address;
 	int				index;
+	int16_t			arg1;
+	int				value;
+	int				idx_address;
 
-	i = -1;
-	arg1 = 0;
-	mult = 256;
-	while (++i < 2)
+	arg1 = ft_bigendian16_read(vm->arena + ((game->procs->pos + 2) % CW_MEM_SIZE));
+	idx_address = (game->procs->pos + arg1) % CW_MEM_SIZE;
+	if (idx_address < 0)
+		idx_address += CW_MEM_SIZE;
+	index = vm->arena[(game->procs->pos + 4) % CW_MEM_SIZE];
+	if (cw_vm_is_reg(index) == true)
 	{
-		arg1 += mult * vm->arena[(game->procs->pos + 2 + i) % CW_MEM_SIZE];
-		mult /= 256;
+		value = ft_bigendian32_read(vm->arena + idx_address);
+		game->procs->regs[index - 1] = value;
+		game->procs->carry = (value == 0) ? 1 : 0;
 	}
-	to_address = (game->procs->pos + arg1) % CW_MEM_SIZE;
-	(to_address < 0) ? to_address += CW_MEM_SIZE : 0;
-	i = -1;
-	arg1 = 0;
-	mult = ft_pow(256, 3);
-	while (++i < 4)
-	{
-		arg1 += mult * vm->arena[(to_address + i) % CW_MEM_SIZE];
-		mult /= 256;
-	}
-	index = vm->arena[(game->procs->pos + 2 + i) % CW_MEM_SIZE];
-	game->procs->regs[index - 1] = arg1;
-	game->procs->carry = (arg1 == 0) ? 1 : 0;
 }
 
 void				cw_vm_op_lld(t_cw_inst *inst, t_cw_game *game, t_cw_vm *vm)
 {
-	if (inst->args_count == 2 && inst->types[0] != T_REG && inst->types[1] == T_REG)
+	if (inst->args_count >= 2 && inst->types[0] != T_REG && inst->types[1] == T_REG)
 	{
 		if (inst->types[0] == T_DIR)
 			cw_vm_op_lld_dir_exec(game, vm);
@@ -77,8 +60,6 @@ void				cw_vm_op_lld(t_cw_inst *inst, t_cw_game *game, t_cw_vm *vm)
 	// opc + encoding byte + (T_DIR | T_IND) + T_REG
 	// 7 T_DIR T_REG
 	// 5 T_IND T_REG
-	game->procs->pos += (1 + 1) % CW_MEM_SIZE;
-	game->procs->pos += ((inst->types[0] == T_DIR) ? CW_DIR_SIZE_LLD : 2) % CW_MEM_SIZE;
-	game->procs->pos += (1) % CW_MEM_SIZE;
+	game->procs->pos = (game->procs->pos + 2 + cw_vm_add_pos(inst, 2, CW_DIR_SIZE_LLD)) % CW_MEM_SIZE;
 	/* ft_printf("0x%02x 0x%02x 0x%02x\n", vm->arena[game->procs->pos-1], vm->arena[game->procs->pos], vm->arena[game->procs->pos + 1]); */
 }
--- srcs/vm/ops/cw_vm_lldi.c
+++ srcs/vm/ops/cw_vm_lldi.c
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/10 17:14:53 by amalsago          #+#    #+#             */
-/*   Updated: 2020/06/13 21:10:07 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/14 20:16:40 by jthierce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,65 +16,31 @@
 
 int					cw_vm_op_lldi_dir(t_cw_game *game, t_cw_vm *vm, int pos)
 {
-	int				i;
-	int 			pow;
-	int				total;
+	int total;
 
-	i = -1;
-	total = 0;
-	pow = ft_pow(256, CW_DIR_SIZE_LLDI - 1);
-	while (++i < CW_DIR_SIZE_LLDI)
-	{
-		total += pow * vm->arena[(game->procs->pos + pos + i) % CW_MEM_SIZE];
-		pow /= 256;
-	}
+	total = ft_bigendian16_read(vm->arena + ((game->procs->pos + pos) % CW_MEM_SIZE));
 	return (total);
 }
 
 int16_t				cw_vm_op_lldi_ind(t_cw_game *game, t_cw_vm *vm, int pos)
 {
-	int				i;
 	int16_t			arg;
 	int				total;
-	unsigned int	mult;
 	int				idx_address;
 
-	i = -1;
-	arg = 0;
-	total = 0;
-	mult = 256;
-	while (++i < 2)
-	{
-		arg += mult * vm->arena[(game->procs->pos + pos + i) % CW_MEM_SIZE];
-		mult /= 256;
-	}
+	arg = ft_bigendian16_read(vm->arena + ((game->procs->pos + pos) % CW_MEM_SIZE));
 	idx_address = (game->procs->pos + (arg % CW_IDX_MOD)) % CW_MEM_SIZE;
 	if (idx_address < 0)
 		idx_address += CW_MEM_SIZE;
-	i = -1;
-	arg = 0;
-	mult = ft_pow(256, 3);
-	while (++i < 3)
-	{
-		total += mult * vm->arena[(idx_address + i) % CW_MEM_SIZE];
-		mult /= 256;
-	}
-	return (arg);
+	total = ft_bigendian32_read(vm->arena + idx_address);
+	return (total);
 }
 
 int					cw_vm_op_lldi_value_reg(t_cw_vm *vm, int pos)
 {
-	int 			i;
-	int 			pow;
-	int 			total;
+	int total;
 
-	i = -1;
-	pow = ft_pow(256, 3);
-	while (++i < 4)
-	{
-		total = vm->arena[(pos + i) % CW_MEM_SIZE];
-		pow /= 256;
-	}
+	total = ft_bigendian32_read(vm->arena + pos);
 	return (total);
 }
 
@@ -90,9 +56,9 @@ void				cw_vm_op_lldi_body(t_cw_inst *inst, t_cw_game *game, t_cw_vm *vm)
 	{
 		if (inst->types[i] == T_REG)
 		{
-			if (cw_vm_is_reg(vm->arena[game->procs->pos + pos]) == false)
-				ft_printf("ERROR\n");
-			arg[i] = game->procs->regs[(vm->arena[game->procs->pos + pos]) - 1];
+			if (cw_vm_is_reg(vm->arena[(game->procs->pos + pos) % CW_MEM_SIZE]) == false)
+				return ;
+			arg[i] = game->procs->regs[(vm->arena[(game->procs->pos + pos) % CW_MEM_SIZE]) - 1];
 			pos++;
 		}
 		else if (inst->types[i] == T_DIR)
@@ -106,24 +72,17 @@ void				cw_vm_op_lldi_body(t_cw_inst *inst, t_cw_game *game, t_cw_vm *vm)
 			pos += 2;
 		}
 	}
-	arg[0] = game->procs->pos + (arg[0] + arg[1]);
+	arg[0] = (game->procs->pos + (arg[0] + arg[1])) % CW_MEM_SIZE;
 	if (arg[0] < 0)
 		arg[0] += CW_MEM_SIZE;
-	game->procs->regs[vm->arena[game->procs->pos + pos] - 1] = cw_vm_op_lldi_value_reg(vm, arg[0]);
+	arg[2] = vm->arena[(game->procs->pos + pos) % CW_MEM_SIZE];
+	if (cw_vm_is_reg(arg[2]))
+		game->procs->regs[arg[2] - 1] = cw_vm_op_lldi_value_reg(vm, arg[0]);
 }
 
 void	cw_vm_op_lldi(t_cw_inst *inst, t_cw_game *game, t_cw_vm *vm)
 {
-	if (inst->args_count == 3 && inst->types[1] != T_IND && inst->types[2] == T_REG)
+	if (inst->args_count >= 3 && inst->types[1] != T_IND && inst->types[2] == T_REG)
 		cw_vm_op_lldi_body(inst, game, vm);
-	// opc + encoding byte + (T_REG | T_DIR | T_IND) + (T_REG | T_DIR) + T_REG
-	// 5 T_REG T_REG
-	// 6 T_REG T_DIR
-	// 6 T_DIR T_REG
-	// 7 T_DIR T_DIR
-	// 6 T_IND T_REG
-	// 7 T_IND T_DIR
-	game->procs->pos += (1 + 1 + ((inst->types[0] == T_REG) ? 1 : CW_DIR_SIZE_LLDI)) % CW_MEM_SIZE;
-	game->procs->pos += ((inst->types[1] == T_REG) ? 1 : CW_DIR_SIZE_LLDI) % CW_MEM_SIZE;
-	game->procs->pos += ((inst->types[2] == T_REG) ? 1 : 0) % CW_MEM_SIZE;
+	game->procs->pos = (game->procs->pos + 2 + cw_vm_add_pos(inst, 3, CW_DIR_SIZE_LLDI)) % CW_MEM_SIZE;
 }
