4708d64f9946aa551b9b23255343e7d4017fd9af
4slan <amalsago@student.42.fr>
2020-06-15 18:50:57 +0200

    feat: add ft_strclen()

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: bleplat <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 10:59:30 by bleplat           #+#    #+#             */
-/*   Updated: 2020/06/08 15:54:27 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/15 18:48:57 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,7 @@ void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t          ft_strclen(const char *s, char c);
 size_t			ft_strlen(const char *s);
 char			*ft_strdup(const char *s1);
 char			*ft_strcpy(char *dst, const char *src);
---
+++ libft/srcs/ft_strclen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strclen.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/26 11:55:55 by amalsago          #+#    #+#             */
+/*   Updated: 2020/06/15 18:50:26 by amalsago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int		ft_isseparator(char c, char separator)
+{
+    return (c == separator);
+}
+
+/*
+** Computes and returns the length of the string s until separator c.
+*/
+
+size_t			ft_strclen(const char *s, char c)
+{
+	size_t		i;
+
+	i = 0;
+	while (s[i] != '\0' && !(ft_isseparator(s[i], c)))
+		i++;
+	return (i);
+}
