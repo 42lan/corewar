3a155567ad01e5d1bd9d70b7cf11b6850c0c0b8e
4slan <amalsago@student.42.fr>
2020-06-15 20:14:46 +0200

    style: normalize header files

--- includes/cw_vm.h
+++ includes/cw_vm.h
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 15:01:47 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/15 05:37:57 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/15 19:51:04 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,24 @@ typedef struct	s_cw_vm
 
 }				t_cw_vm;
 
-t_bool	cw_vm_is_reg(int reg);
-t_bool	cw_vm_is_valid_op(int op);
-void	cw_vm_usage(void);
-int		cw_vm_parsing(int argc, char **argv, t_cw_vm *vm);
-int		cw_vm_get_data(char **argv);
-int		cw_vm_get_player(t_cw_vm *vm, char *filename);
-int		cw_vm_is_valid_extension(const char *argv, const char *extension);
-void	cw_vm_print_data(t_cw_data *data);
-int		cw_vm_read_player(t_cw_vm *vm);
-int		cw_vm_check_nbr_players(t_cw_vm *vm);
-int		cw_vm_ini_arena(t_cw_vm *vm);
-void	cw_vm_intro_players(t_cw_vm *vm);
-int		cw_vm_game(t_cw_vm *vm);
-void	cw_vm_print_data(t_cw_data *data);
-int		cw_vm_set_player(t_cw_vm *vm, int assigned_nbr, char *filename);
-int		cw_vm_set_player_helper(t_cw_vm *vm, int ac, char **av, int *i);
-int		cw_vm_check_nbr_players(t_cw_vm *vm);
-int		cw_vm_set_dump(t_cw_vm *vm, char **av, int *i);
+t_bool			cw_vm_is_reg(int reg);
+t_bool			cw_vm_is_valid_op(int op);
+void			cw_vm_usage(void);
+int				cw_vm_parsing(int argc, char **argv, t_cw_vm *vm);
+int				cw_vm_get_data(char **argv);
+int				cw_vm_get_player(t_cw_vm *vm, char *filename);
+int				cw_vm_is_valid_extension(const char *argv,
+					const char *extension);
+void			cw_vm_print_data(t_cw_data *data);
+int				cw_vm_read_player(t_cw_vm *vm);
+int				cw_vm_check_nbr_players(t_cw_vm *vm);
+int				cw_vm_ini_arena(t_cw_vm *vm);
+void			cw_vm_intro_players(t_cw_vm *vm);
+int				cw_vm_game(t_cw_vm *vm);
+void			cw_vm_print_data(t_cw_data *data);
+int				cw_vm_set_player(t_cw_vm *vm, int assigned_nbr, char *filename);
+int				cw_vm_set_player_helper(t_cw_vm *vm, int ac, char **av, int *i);
+int				cw_vm_check_nbr_players(t_cw_vm *vm);
+int				cw_vm_set_dump(t_cw_vm *vm, char **av, int *i);
 
 #endif
--- includes/cw_vm_player.h
+++ includes/cw_vm_player.h
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 14:38:41 by jthierce          #+#    #+#             */
-/*   Updated: 2020/06/15 18:09:41 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/06/15 19:52:21 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,20 @@
 ** number	- player's unique ID number
 */
 
-typedef struct		s_cw_player
+typedef struct	s_cw_player
 {
 	t_cw_champion	*champion;
 	int				number;
 	int				init_pos;
-}					t_cw_player;
+}				t_cw_player;
 
-int		cw_parsing_number_player(t_cw_data *data);
-int		cw_vm_valid_player(t_cw_data *data, t_cw_player *players);
-int		cw_vm_read_magic_number(int fd);
-int		cw_vm_read_champion_name(int fd, t_cw_player *player);
-int		cw_vm_read_champion_null(int fd, char *part);
-int		cw_vm_read_exec_code_len(int fd, t_cw_player *player);
-int		cw_vm_read_champion_comment(int fd, t_cw_player *player);
-int		cw_vm_read_champion_exec_code(int fd, t_cw_player *player);
+int				cw_parsing_number_player(t_cw_data *data);
+int				cw_vm_valid_player(t_cw_data *data, t_cw_player *players);
+int				cw_vm_read_magic_number(int fd);
+int				cw_vm_read_champion_name(int fd, t_cw_player *player);
+int				cw_vm_read_champion_null(int fd, char *part);
+int				cw_vm_read_exec_code_len(int fd, t_cw_player *player);
+int				cw_vm_read_champion_comment(int fd, t_cw_player *player);
+int				cw_vm_read_champion_exec_code(int fd, t_cw_player *player);
 
 #endif
