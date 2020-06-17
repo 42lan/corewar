6197d1942dd9f0a6a90d17358f0a4793f0539422
bleplat <bleplat@student.42.fr>
2020-06-17 19:06:01 +0200

    norme

--- includes/cw_asm.h
+++ includes/cw_asm.h
@@ -6,7 +6,7 @@
 /*   By: bleplat <bleplat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 18:17:34 by bleplat           #+#    #+#             */
-/*   Updated: 2020/06/17 18:17:43 by bleplat          ###   ########.fr       */
+/*   Updated: 2020/06/17 19:04:38 by bleplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@
 # include "cw_linst.h"
 # include "cw_asm_options.h"
 
-#define CW_COMMENT_CHAR '#'
-#define CW_COMMENT_CHAR2 ';'
-#define CW_LABEL_CHAR ':'
-#define CW_DIRECT_CHAR '%'
-#define CW_SEP_CHAR ','
-#define CW_LABEL_CHARS "abcdefghijklmnopqrstuvwxyz_0123456789"
+# define CW_COMMENT_CHAR '#'
+# define CW_COMMENT_CHAR2 ';'
+# define CW_LABEL_CHAR ':'
+# define CW_DIRECT_CHAR '%'
+# define CW_SEP_CHAR ','
+# define CW_LABEL_CHARS "abcdefghijklmnopqrstuvwxyz_0123456789"
 
 /*
 ** t_cw_asm:
@@ -50,50 +50,54 @@ struct					s_cw_asm
 	t_cw_champion		*champion;
 };
 
-t_cw_asm		*cw_asm_create(void);
-void			cw_asm_destroy(t_cw_asm **state);
+t_cw_asm				*cw_asm_create(void);
+void					cw_asm_destroy(t_cw_asm **state);
 
 /*
 ** Errors:
 */
 
-int				cw_asm_error(int code);
-int				cw_asmr(t_cw_asm *state, t_cw_linst *linst,
-						int error, int index);
+int						cw_asm_error(int code);
+int						cw_asmr(t_cw_asm *state, t_cw_linst *linst,
+								int error, int index);
 
 /*
 ** Main asm algorithm:
 */
 
-int				cw_asm_run(t_cw_asm *state);
+int						cw_asm_run(t_cw_asm *state);
 
-int				cw_asm_input(t_cw_asm *state);
-int				cw_asm_translate(t_cw_asm *state);
-int				cw_asm_translate_labels(t_cw_asm *state);
-int				cw_asm_translate_insts(t_cw_asm *state);
-int				cw_asm_translate_insts_labels(t_cw_asm *state);
-int				cw_asm_assemble(t_cw_asm *state);
-int				cw_asm_output(t_cw_asm *state);
+int						cw_asm_input(t_cw_asm *state);
+int						cw_asm_translate(t_cw_asm *state);
+int						cw_asm_translate_labels(t_cw_asm *state);
+int						cw_asm_translate_insts(t_cw_asm *state);
+int						cw_asm_translate_insts_labels(t_cw_asm *state);
+int						cw_asm_assemble(t_cw_asm *state);
+int						cw_asm_output(t_cw_asm *state);
 
 /*
 ** Helpers:
 */
 
-int				cw_asm_get_label_index(t_cw_asm *state, char *name);
-int				cw_asm_get_label_offset(t_cw_asm *state, int index);
-int				cw_asm_nothing_at_end(t_cw_asm *state, t_cw_linst *linst,
-									 int index);
-int				cw_asm_dismember_string(t_cw_asm *state, t_cw_linst *linst,
-									 int index);
+int						cw_asm_get_label_index(t_cw_asm *state, char *name);
+int						cw_asm_get_label_offset(t_cw_asm *state, int index);
+int						cw_asm_nothing_at_end(t_cw_asm *state,
+												t_cw_linst *linst,
+												int index);
+int						cw_asm_dismember_string(t_cw_asm *state,
+												t_cw_linst *linst,
+												int index);
 
-int				cw_asm_skip_label_index(t_cw_linst *linst);
-int				cw_asm_skip_spaces_index(t_cw_linst *linst, int index);
-int				cw_asm_spaces_index(t_cw_linst *linst, int index);
-int				cw_asm_skip_alnums_index(t_cw_linst *linst, int index);
-int				cw_asm_argend_index(t_cw_linst *linst, int index);
-int				cw_asm_nextarg_index(t_cw_linst *linst, int index);
+int						cw_asm_skip_label_index(t_cw_linst *linst);
+int						cw_asm_skip_spaces_index(t_cw_linst *linst, int index);
+int						cw_asm_spaces_index(t_cw_linst *linst, int index);
+int						cw_asm_skip_alnums_index(t_cw_linst *linst, int index);
+int						cw_asm_argend_index(t_cw_linst *linst, int index);
+int						cw_asm_nextarg_index(t_cw_linst *linst, int index);
 
-int				cw_asm_translate_literal(t_cw_asm *state, t_cw_linst *linst);
-int				cw_asm_translate_inst(t_cw_asm *state, t_cw_linst *linst);
+int						cw_asm_translate_literal(t_cw_asm *state,
+													t_cw_linst *linst);
+int						cw_asm_translate_inst(t_cw_asm *state,
+												t_cw_linst *linst);
 
 #endif
--- includes/cw_literal.h
+++ includes/cw_literal.h
@@ -6,7 +6,7 @@
 /*   By: bleplat <bleplat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/17 18:20:37 by bleplat           #+#    #+#             */
-/*   Updated: 2020/06/17 18:20:39 by bleplat          ###   ########.fr       */
+/*   Updated: 2020/06/17 19:01:48 by bleplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 /*
 ** t_cw_literal
 **
-**
+** type: literal type, like name or comment.
+** value: the value given as parameter for the literal.
 */
 
 # define CW_LITERAL_NAME ".name"
@@ -41,12 +42,12 @@ struct						s_cw_literal
 	char			*value;
 };
 
-t_cw_literal		*cw_literal_create(int type, char *value);
-void				cw_literal_destroy(t_cw_literal **literal);
+t_cw_literal				*cw_literal_create(int type, char *value);
+void						cw_literal_destroy(t_cw_literal **literal);
 
-int					cw_literal_get_type(const char *instr);
-int					cw_literal_size(t_cw_literal *literal);
-void				cw_literal_write(t_cw_literal *literal,
+int							cw_literal_get_type(const char *instr);
+int							cw_literal_size(t_cw_literal *literal);
+void						cw_literal_write(t_cw_literal *literal,
 										unsigned char *out);
 
 #endif
