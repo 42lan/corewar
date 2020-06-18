f7059522c48909beac9152e8d3d2a38f262e45a3
user <user@127.0.0.1>
2020-06-18 15:13:51 +0200

    -

--- libft/srcs/ft_bzero.c
+++ libft/srcs/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: bleplat <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 12:58:30 by bleplat           #+#    #+#             */
-/*   Updated: 2018/11/08 12:16:57 by bleplat          ###   ########.fr       */
+/*   Updated: 2020/06/18 15:13:44 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,6 @@
 
 void			ft_bzero(void *s, size_t len)
 {
-	while (len > sizeof(long))
-	{
-		len -= sizeof(long);
-		*(long *)((char *)s + len) = 0L;
-	}
-	while (len > sizeof(int))
-	{
-		len -= sizeof(int);
-		*(int *)((char *)s + len) = (int)0;
-	}
 	while (len > 0)
 	{
 		len -= 1;
